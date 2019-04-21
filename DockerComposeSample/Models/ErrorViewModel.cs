6d1fe9218cbd6d266395d6b726f7d8b74783a1b0
zw lin <1130120850@qq.com>
2019-04-21 16:47:07 +0800

    init

---
+++ DockerComposeSample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DockerComposeSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
